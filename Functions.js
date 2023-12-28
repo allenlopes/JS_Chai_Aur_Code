182f2b4005eac87598bce3b055d42c5521ed940b
sudoallenlopes23 <allenlopes10@gmail.com>
2023-12-29 01:32:21 +0530

    JavaScript Functions..

--- Functions.js
+++ Functions.js
@@ -15,3 +15,33 @@ let emp2 = {
 };
 let x = employee.details.call(emp2, "Manager", "4 years");
 console.log(x);
+
+//// SOME COMPLEX
+
+
+// Function to calculate the factorial of a number using recursion
+function factorial(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+// Function to find the nth Fibonacci number using iteration
+function fibonacci(n) {
+  let fibArray = [0, 1];
+
+  for (let i = 2; i <= n; i++) {
+    fibArray[i] = fibArray[i - 1] + fibArray[i - 2];
+  }
+
+  return fibArray[n];
+}
+
+// Example usage
+const numberForFactorial = 5;
+const numberForFibonacci = 7;
+
+console.log(`Factorial of ${numberForFactorial}: ${factorial(numberForFactorial)}`);
+console.log(`Fibonacci number at position ${numberForFibonacci}: ${fibonacci(numberForFibonacci)}`);
