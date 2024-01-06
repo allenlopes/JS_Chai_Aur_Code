4b17b4a6bdf60d51e61edf167ae22f3562626247
sudoallenlopes23 <allenlopes10@gmail.com>
2024-01-06 20:37:11 +0530

    In Functions.js some edits.

--- Functions.js
+++ Functions.js
@@ -67,3 +67,13 @@ asyncTask(true)
     .catch((error) => {
         console.error(error);
     });
+
+
+    // program to find the square of a number
+// function is declared inside the variable
+let x = function (num) { return num * num };
+console.log(x(4));
+
+// can be used as variable value for other variables
+let y = x(3);
+console.log(y);
