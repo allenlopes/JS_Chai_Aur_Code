a4c5b03235b32c16fccfdd678452a03199fbd771
sudoallenlopes23 <allenlopes10@gmail.com>
2024-01-09 21:48:59 +0530

    In Functions.js, some edits.

--- Functions.js
+++ Functions.js
@@ -101,3 +101,21 @@ console.log(y);
 
 var pow = new Function("num1", "num2", "return Math.pow(num1,num2)");
 document.writeln(pow(2, 3));
+
+/////
+
+/*
+function abc() {
+let x = 30;
+let y = 40;
+document.write(x * y);
+}
+abc();
+*/
+
+// Anonymous function
+(function () {
+  let x = 10;
+  let y = 20;
+  document.write(x * y);
+}) ();  // () this indicated function is called.
