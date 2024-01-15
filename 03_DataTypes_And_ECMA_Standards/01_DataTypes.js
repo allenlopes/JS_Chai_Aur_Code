6f53640f5b31f6bee25ff7c5d3e42c901b4a04fe
sudoallenlopes23 <allenlopes10@gmail.com>
2024-01-15 23:04:03 +0530

    In data types, some edits.

--- 03_DataTypes_And_ECMA_Standards/01_DataTypes.js
+++ 03_DataTypes_And_ECMA_Standards/01_DataTypes.js
@@ -1,21 +1,5 @@
 "use strict"; // treat all JS code as newer version
 
-let num = 2; // Integer
-let num2 = 1.3; // Floating point number
-let num3 = Infinity; // Infinity
-let num4 = 'something here too'/2; // NaN
-
-// Numbers:
-let length = 16;
-let weight = 7.5;
-
-// Strings:
-let color = "Yellow";
-let lastName = "Johnson";
-
-let str = "Hello There";
-let str2 = "Single quotes works fine";
-let phrase = `can embed ${str}`;
 
 // Booleans
 let x = true;
