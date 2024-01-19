cd2073e0e86d40c2ef5bdb72f9bbb8a1b065643b
sudoallenlopes23 <allenlopes10@gmail.com>
2024-01-19 23:48:45 +0530

    In functions.js..

--- Functions.js
+++ Functions.js
@@ -158,3 +158,10 @@ function fetchData() {
 fetchData()
     .then(data => console.log(data))
     .catch(error => console.error(error));
+
+
+//// Transform a function with multiple arguments into a sequence of functions with a single argument
+
+const add = (a) => (b) => a + b;
+const add5 = add(5);
+console.log(add5(3)); // Output: 8
