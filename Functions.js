fec0d62f93abcf3918c3e1ce6aa46a6e86fb499a
sudoallenlopes23 <allenlopes10@gmail.com>
2024-02-08 23:39:29 +0530

    In functions..

--- Functions.js
+++ Functions.js
@@ -252,3 +252,21 @@ function factorial(n) {
         return n * factorial(n - 1);
     }
 }
+
+//
+// program to count down numbers to 1
+function countDown(number) {
+
+    // display the number
+    console.log(number);
+
+    // decrease the number value
+    const newNumber = number - 1;
+
+    // base case
+    if (newNumber > 0) {
+        countDown(newNumber);
+    }
+}
+
+countDown(4);
