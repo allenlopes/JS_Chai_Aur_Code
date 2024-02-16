ef4f33dee450278b88f2b89240403f6178694e34
sudoallenlopes23 <allenlopes10@gmail.com>
2024-02-16 23:05:46 +0530

    In functions..

--- Functions.js
+++ Functions.js
@@ -3,19 +3,6 @@ function greet(name) {
     return "Hello, " + name + "!";
 }
 
-// Function expression
-var add = function(x, y) {
-    return x + y;
-};
-
-// Arrow function
-const multiply = (a, b) => a * b;
-
-// Calling the functions
-console.log(greet("John")); // Output: Hello, John!
-console.log(add(5, 3));      // Output: 8
-console.log(multiply(4, 6)); // Output: 24
-
 
 ///////////
 
